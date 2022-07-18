fd2e0f46f5e9598434dd3e2885b9b7a69802ec3f
geraldgillespie <gerald.gillespie@inscio.com>
2022-07-18 15:56:37 -0400

    add concept of root and ability to hold text

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,72 +1,79 @@
-﻿import { AST, SourceCode } from 'eslint';
-import { Statement, ModuleDeclaration } from 'estree';
-import { ScopeManager } from 'eslint-scope';
+﻿import {AST, SourceCode}              from 'eslint';
+import {Statement, ModuleDeclaration} from 'estree';
+import {ScopeManager}                 from 'eslint-scope';
 
 export type XmlTokenType =
-    | 'XmlAttribute'
-    | 'XmlText'
-    | 'XmlElement'
-    | 'XmlComment'
-    | 'Program'
+  | 'XmlAttribute'
+  | 'XmlText'
+  | 'XmlElement'
+  | 'XmlRoot'
+  | 'XmlComment'
+  | 'Program'
 
 export interface ParserOptions {
-        xmlMode? : boolean;
-        decodeEntities? : boolean;
-        lowerCaseTags?: boolean;
-        lowerCaseAttributeNames?: boolean;
-        recognizeSelfClosing?: boolean;
+  xmlMode?: boolean;
+  decodeEntities?: boolean;
+  lowerCaseTags?: boolean;
+  lowerCaseAttributeNames?: boolean;
+  recognizeSelfClosing?: boolean;
 }
 
 export interface ESLintXmlParserToken {
-    type: XmlTokenType | AST.TokenType;
-    raw: string;
-    value: string;
-    range: AST.Range;
-    loc: AST.SourceLocation;
+  type: XmlTokenType | AST.TokenType;
+  raw: string;
+  value: string;
+  range: AST.Range;
+  loc: AST.SourceLocation;
 }
 
 export interface XmlAttribute extends ESLintXmlParserToken {
-    type: 'XmlAttribute';
-    parent: XmlElement;
-    attribName : string;
-    attribValue: string;
+  type: 'XmlAttribute';
+  parent: XmlElement;
+  attribName: string;
+  attribValue: string;
 }
 
 export interface XmlText extends ESLintXmlParserToken {
-    type: 'XmlText';
-    parent: XmlElement;
-    text : string;
+  type: 'XmlText';
+  parent: XmlElement;
+  text: string;
 }
 
 
 export interface XmlComment extends ESLintXmlParserToken {
-    type: 'XmlComment';
-    parent: XmlElement;
-    comment: string;
+  type: 'XmlComment';
+  parent: XmlElement;
+  comment: string;
 }
 
 
 export interface XmlElement extends ESLintXmlParserToken {
-    comments: string[];
-    type: 'XmlElement';
-    tagName: string;
-    parent?: XmlElement;
-    attributes: XmlAttribute[];
-    children: (XmlElement | XmlText | XmlComment | Statement | ModuleDeclaration)[];
+  comments: string[];
+  type: 'XmlElement';
+  tagName: string;
+  canHoldText: boolean;
+  parent: XmlElement;
+  attributes: XmlAttribute[];
+  children: (XmlElement | XmlText | XmlComment | Statement | ModuleDeclaration)[];
+}
+
+export interface XmlRoot extends XmlElement {
+  type: 'XmlElement'; // keep it as XmlElement
+  parent: null;
 }
 
 export interface XmlSyntaxTree extends ESLintXmlParserToken {
-    comments: any[];
-    tokens: ESLintXmlParserToken[];
-    root: XmlElement;
-    type: 'Program'
+  comments: any[];
+  tokens: ESLintXmlParserToken[];
+  root: XmlRoot;
+  type: 'Program'
 }
 
 export interface ESLintXmlParseResult {
-    ast: XmlSyntaxTree | AST.Program;
-    services?: Object;
-    scopeManager?: ScopeManager;
-    visitorKeys?: SourceCode.VisitorKeys;
+  ast: XmlSyntaxTree | AST.Program;
+  services?: Object;
+  scopeManager?: ScopeManager;
+  visitorKeys?: SourceCode.VisitorKeys;
 }
 
 export function parseForESLint(code: string, options?: any): ESLintXmlParseResult;
